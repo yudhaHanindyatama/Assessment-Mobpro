3b390e8e036a2cf195df20db7ca339dc4c2fba25
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-04-01 10:35:40 +0700

    Membuat TopAppBar

--- app/src/main/java/org/d3if3053/assessment/MainActivity.kt
+++ app/src/main/java/org/d3if3053/assessment/MainActivity.kt
@@ -1,14 +1,21 @@
 package org.d3if3053.assessment
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import org.d3if3053.assessment.ui.theme.AssessmentTheme
 
@@ -29,17 +36,39 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(content: @Composable (Modifier) -> Unit) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { padding ->
+        content(Modifier.padding(padding))
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    MainScreen {modifier ->
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ScreenPreview() {
     AssessmentTheme {
         Greeting("Android")
     }
