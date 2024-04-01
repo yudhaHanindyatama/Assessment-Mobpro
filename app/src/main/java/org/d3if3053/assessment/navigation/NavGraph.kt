cf88c397335cc3f57efc3215f72f5d62c14982ab
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-04-01 11:23:00 +0700

    Membuat navigasi dengan tombol Up

--- app/src/main/java/org/d3if3053/assessment/navigation/NavGraph.kt
+++ app/src/main/java/org/d3if3053/assessment/navigation/NavGraph.kt
@@ -18,7 +18,7 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
             MainScreen(navController)
         }
         composable(route = Screen.About.route) {
-            AboutScreen()
+            AboutScreen(navController)
         }
     }
 }
--- app/src/main/java/org/d3if3053/assessment/ui/screen/AboutScreen.kt
+++ app/src/main/java/org/d3if3053/assessment/ui/screen/AboutScreen.kt
@@ -2,7 +2,11 @@ package org.d3if3053.assessment.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,15 +17,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if3053.assessment.R
 import org.d3if3053.assessment.ui.theme.AssessmentTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen() {
+fun AboutScreen(navController: NavHostController) {
     Scaffold (
         topBar = {
             TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = stringResource(id = R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
                 title = { Text(text = stringResource(id = R.string.tentang_aplikasi))
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
@@ -33,7 +48,9 @@ fun AboutScreen() {
     ) { padding ->
         Text(
             text = stringResource(id = R.string.copyright),
-            modifier = Modifier.padding(padding).padding(16.dp)
+            modifier = Modifier
+                .padding(padding)
+                .padding(16.dp)
         )
     }
 }
@@ -43,6 +60,6 @@ fun AboutScreen() {
 @Composable
 fun AboutScreenPreview() {
     AssessmentTheme {
-        AboutScreen()
+        AboutScreen(rememberNavController())
     }
 }
