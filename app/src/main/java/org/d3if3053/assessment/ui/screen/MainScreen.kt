54d62b31cc436ba8e4f0bc3ad50672b8989b1a55
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-04-01 11:17:36 +0700

    Membuat navigasi menggunakan menu

--- app/src/main/java/org/d3if3053/assessment/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3053/assessment/ui/screen/MainScreen.kt
@@ -4,7 +4,11 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,12 +19,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import org.d3if3053.assessment.R
+import org.d3if3053.assessment.navigation.Screen
 import org.d3if3053.assessment.ui.theme.AssessmentTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen() {
+fun MainScreen(navController: NavHostController) {
     Scaffold (
         topBar = {
             TopAppBar(
@@ -29,7 +36,20 @@ fun MainScreen() {
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
-                )
+                ),
+                actions = {
+                    IconButton(
+                        onClick = {
+                            navController.navigate(Screen.About.route)
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Info,
+                            contentDescription = stringResource(id = R.string.tentang_aplikasi),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             )
         }
     ) { padding ->
@@ -53,6 +73,6 @@ fun ScreenContent(modifier: Modifier) {
 @Composable
 fun ScreenPreview() {
     AssessmentTheme {
-        MainScreen()
+        MainScreen(rememberNavController())
     }
 }
