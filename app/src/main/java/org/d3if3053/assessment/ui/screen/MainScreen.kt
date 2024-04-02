d7d97ba9002173a3aaefe05e804c56ae9b9874b0
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-04-02 13:24:48 +0700

    Membuat input data untuk user dan error handling

--- app/src/main/java/org/d3if3053/assessment/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3053/assessment/ui/screen/MainScreen.kt
@@ -1,22 +1,45 @@
 package org.d3if3053.assessment.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.animation.expandVertically
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -57,14 +80,128 @@ fun MainScreen(navController: NavHostController) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScreenContent(modifier: Modifier) {
+    var uang by remember {
+        mutableStateOf("")
+    }
+    var uangError by remember {
+        mutableStateOf(false)
+    }
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    val mataUang = listOf(
+        stringResource(id = R.string.dollar),
+        stringResource(id = R.string.euro),
+        stringResource(id = R.string.pondsterling)
+    )
+    var selectedText by remember {
+        mutableStateOf(mataUang[0])
+    }
+    var hasilMataUang by remember {
+        mutableStateOf("")
+    }
+    val conversionRates = mapOf(
+        stringResource(id = R.string.dollar) to 0.00629,
+        stringResource(id = R.string.euro) to 0.00588,
+        stringResource(id = R.string.pondsterling) to 0.00072
+    )
+    var hasil by remember { mutableFloatStateOf(0f) }
+
     Column (
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        OutlinedTextField(
+            value = uang,
+            onValueChange = { uang = it },
+            label = { Text(text = stringResource(id = R.string.uang))},
+            singleLine = true,
+            isError = uangError,
+            trailingIcon = { IconPicker(isError = uangError)},
+            supportingText = { ErrorHint(isError = uangError) },
+            leadingIcon = { Text(text = stringResource(id = R.string.rp))},
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+            OutlinedTextField(
+                value = selectedText,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                label = { Text(text = stringResource(id = R.string.mata_uang))},
+                modifier = Modifier.menuAnchor()
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false}
+            ) {
+                mataUang.forEach { item ->
+                    DropdownMenuItem(
+                        text = { Text(text = item) },
+                        onClick = {
+                            selectedText = item
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
+        Button(
+            onClick = {
+                uangError = (uang == "" || uang == "0")
+                if (uangError) return@Button
 
+                val jumlah = uang.toFloatOrNull()
+                val rate = conversionRates[selectedText]
+                if (jumlah != null && rate != null) {
+                    val result = jumlah * rate
+                    hasil = result.toFloat()
+                }
+                hasilMataUang = selectedText
+            },
+            modifier = Modifier.padding(12.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = stringResource(id = R.string.konversi))
+        }
+        if (hasil != 0f) {
+            Divider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                thickness = 1.dp
+            )
+            Text(
+                text = stringResource(id = R.string.hasil, hasil, hasilMataUang),
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+    }
+}
+
+@Composable
+fun IconPicker(isError: Boolean) {
+    if (isError) {
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+    } else {
+    }
+}
+
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(text = stringResource(id = R.string.input_invalid))
     }
 }
 
