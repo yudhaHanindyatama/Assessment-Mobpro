eb1577954df4c699823aabe97cd01ccedf017664
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-05-05 15:20:12 +0700

    Mengambil data dari database

---
+++ app/src/main/java/org/d3if3053/assessment/ui/screen/MainViewModel.kt
@@ -0,0 +1,17 @@
+package org.d3if3053.assessment.ui.screen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import org.d3if3053.assessment.database.KeuanganDao
+import org.d3if3053.assessment.model.Keuangan
+
+class MainViewModel(dao: KeuanganDao) : ViewModel() {
+    val data: StateFlow<List<Keuangan>> = dao.getKeuangan().stateIn(
+        scope =viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000L),
+        initialValue = emptyList()
+    )
+}
