f337522b80bc68be8537281f95d6035dc86ecd1d
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-05-05 15:22:05 +0700

    Membuat display alert dialog

---
+++ app/src/main/java/org/d3if3053/assessment/ui/screen/DisplayAlertDialog.kt
@@ -0,0 +1,47 @@
+package org.d3if3053.assessment.ui.screen
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import org.d3if3053.assessment.R
+import org.d3if3053.assessment.ui.theme.AssessmentTheme
+
+@Composable
+fun DisplayAlertDialog(
+    openDialog: Boolean,
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit
+) {
+    if (openDialog) {
+        AlertDialog(
+            text = { Text(text = stringResource(R.string.pesan_hapus))},
+            confirmButton = {
+                TextButton(onClick = { onConfirmation() }) {
+                    Text(text = stringResource(R.string.tombol_hapus))
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { onDismissRequest() }) {
+                    Text(text = stringResource(R.string.tombol_batal))
+                }
+            },
+            onDismissRequest = { onDismissRequest() }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DialogPreview() {
+    AssessmentTheme {
+        DisplayAlertDialog(
+            openDialog = true,
+            onDismissRequest = {},
+            onConfirmation = {}
+        )
+    }
+}
