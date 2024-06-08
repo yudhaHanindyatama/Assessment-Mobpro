7e251bfe37b74cceadcdf2fbabcd1b6cae13de2d
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-06-08 12:43:40 +0700

    Menambah model karya seni

---
+++ app/src/main/java/org/d3if3053/assessment/model/Art.kt
@@ -0,0 +1,7 @@
+package org.d3if3053.assessment.model
+
+data class Art(
+    val judul: String,
+    val deskripsi: String,
+    val tahun: Int
+)
--- app/src/main/java/org/d3if3053/assessment/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if3053/assessment/ui/screen/MainViewModel.kt
@@ -1,16 +1,7 @@
 package org.d3if3053.assessment.ui.screen
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
-import org.d3if3053.assessment.database.KeuanganDao
 
-class MainViewModel(dao: KeuanganDao) : ViewModel() {
-    val data: StateFlow<List<Keuangan>> = dao.getKeuangan().stateIn(
-        scope =viewModelScope,
-        started = SharingStarted.WhileSubscribed(5000L),
-        initialValue = emptyList()
-    )
+class MainViewModel() : ViewModel() {
+
 }
