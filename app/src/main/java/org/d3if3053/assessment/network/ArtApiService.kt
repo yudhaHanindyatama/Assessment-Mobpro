29846dd4523a9ce0b753cecddb1295260d520a10
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-06-08 12:54:23 +0700

    Menghubungkan aplikasi dengan internet

---
+++ app/src/main/java/org/d3if3053/assessment/network/ArtApiService.kt
@@ -0,0 +1,19 @@
+package org.d3if3053.assessment.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+private const val BASE_URL = ""
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+interface ArtApiService {
+}
