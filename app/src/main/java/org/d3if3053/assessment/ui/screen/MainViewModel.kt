765a1acc344e25ee7037fbd6feb064aafd05944a
Yudha Hanindyatama <yudhahanindyatama21@gmail.com>
2024-06-12 22:49:37 +0700

    Membuat main view model untuk akses data dari server

--- app/src/main/java/org/d3if3053/assessment/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if3053/assessment/ui/screen/MainViewModel.kt
@@ -1,7 +1,89 @@
 package org.d3if3053.assessment.ui.screen
 
+import android.graphics.Bitmap
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.d3if3053.assessment.model.Art
+import org.d3if3053.assessment.network.ApiStatus
+import org.d3if3053.assessment.network.ArtApi
+import java.io.ByteArrayOutputStream
 
-class MainViewModel() : ViewModel() {
+class MainViewModel : ViewModel() {
 
+    var data = mutableStateOf(emptyList<Art>())
+        private set
+    var status = MutableStateFlow(ApiStatus.LOADING)
+        private set
+    var errorMessage = mutableStateOf<String?>(null)
+        private set
+
+    fun retrieveData(userId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            status.value = ApiStatus.LOADING
+            try {
+                data.value = ArtApi.service.getArt(userId)
+                status.value = ApiStatus.SUCCESS
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Failed: ${e.message}")
+                status.value = ApiStatus.FAILED
+            }
+        }
+    }
+
+    fun saveData(userId: String, judul: String, deskripsi: String, tahun: String, bitmap: Bitmap) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val result = ArtApi.service.postArt(
+                    userId,
+                    judul.toRequestBody("text/plain".toMediaTypeOrNull()),
+                    deskripsi.toRequestBody("text/plain".toMediaTypeOrNull()),
+                    tahun.toRequestBody("text/plain".toMediaTypeOrNull()),
+                    bitmap.toMultipartBody()
+                )
+                if (result.status == "success")
+                    retrieveData(userId)
+                else
+                    throw Exception(result.message)
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Failure: ${e.message}")
+                errorMessage.value = "Error: ${e.message}"
+            }
+        }
+    }
+
+    fun deleteArt(userId: String, hewanId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val result = ArtApi.service.delete(userId, hewanId)
+                if (result.status == "success") {
+                    retrieveData(userId)
+                } else {
+                    throw Exception(result.message)
+                }
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Failed to delete: ${e.message}")
+                errorMessage.value = "Error: ${e.message}"
+            }
+        }
+    }
+
+    private fun Bitmap.toMultipartBody(): MultipartBody.Part {
+        val stream = ByteArrayOutputStream()
+        compress(Bitmap.CompressFormat.JPEG, 80, stream)
+        val byteArray = stream.toByteArray()
+        val requestBody = byteArray.toRequestBody(
+            "image/jpg".toMediaTypeOrNull(), 0, byteArray.size)
+        return MultipartBody.Part.createFormData(
+            "image", "image.jpg", requestBody)
+    }
+
+    fun clearMessage() { errorMessage.value = null }
 }
